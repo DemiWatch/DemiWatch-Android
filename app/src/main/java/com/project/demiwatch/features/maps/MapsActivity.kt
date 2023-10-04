fb8ff45e1c08f6973ca76eef5a3966b866ca0ead
Krisna <krisnaerlangga08@gmail.com>
2023-10-04 13:36:00 +0700

    add: setup folder and themes :rocket:

---
+++ app/src/main/java/com/project/demiwatch/features/maps/MapsActivity.kt
@@ -0,0 +1,12 @@
+package com.project.demiwatch.features.maps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.project.demiwatch.R
+
+class MapsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+    }
+}
