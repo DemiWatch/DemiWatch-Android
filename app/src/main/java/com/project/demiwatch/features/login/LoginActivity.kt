bb565eaf6840d7c4ffe69a2885395b988dfb771b
Krisna <krisnaerlangga08@gmail.com>
2023-10-11 15:24:14 +0700

    add: register layout

--- app/src/main/java/com/project/demiwatch/features/login/LoginActivity.kt
+++ app/src/main/java/com/project/demiwatch/features/login/LoginActivity.kt
@@ -1,10 +1,12 @@
 package com.project.demiwatch.features.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.project.demiwatch.R
 import com.project.demiwatch.databinding.ActivityLoginBinding
 import com.project.demiwatch.databinding.ActivitySplashBinding
+import com.project.demiwatch.features.register.RegisterActivity
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
--- app/src/main/java/com/project/demiwatch/features/register/RegisterActivity.kt
+++ app/src/main/java/com/project/demiwatch/features/register/RegisterActivity.kt
@@ -3,10 +3,14 @@ package com.project.demiwatch.features.register
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.project.demiwatch.R
+import com.project.demiwatch.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
