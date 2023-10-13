021ecd40b1d179d53ae1397a505c21ebb7718c5f
Krisna <krisnaerlangga08@gmail.com>
2023-10-13 13:47:36 +0700

    add: implement form logic register and login

--- app/src/main/java/com/project/demiwatch/features/login/LoginActivity.kt
+++ app/src/main/java/com/project/demiwatch/features/login/LoginActivity.kt
@@ -3,9 +3,15 @@ package com.project.demiwatch.features.login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Patterns
+import android.view.View
 import com.project.demiwatch.R
+import com.project.demiwatch.core.utils.showLongToast
 import com.project.demiwatch.databinding.ActivityLoginBinding
 import com.project.demiwatch.databinding.ActivitySplashBinding
+import com.project.demiwatch.features.home.HomeActivity
 import com.project.demiwatch.features.register.RegisterActivity
 
 class LoginActivity : AppCompatActivity() {
@@ -17,13 +23,65 @@ class LoginActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
+        setupEditTextFormat()
+
         setButtonToRegister()
 
         setButtonLogin()
     }
 
-    private fun setButtonLogin() {
+    private fun setupEditTextFormat() {
+        binding.edEmail.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                if (!Patterns.EMAIL_ADDRESS.matcher(p0!!).matches()) {
+                    binding.edEmail.error = getString(R.string.invalid_email)
+                    binding.btnLogin.isEnabled = false
+                } else binding.btnLogin.isEnabled =
+                    !(binding.edPass.text.isNullOrEmpty())
+            }
+        })
+
+        binding.edPass.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                if (p0.toString().length < 8) {
+                    binding.edPass.error = getString(R.string.invalid_password)
+                    binding.btnLogin.isEnabled = false
+                } else binding.btnLogin.isEnabled =
+                    !(binding.edEmail.text.isNullOrEmpty())
+            }
+        })
+    }
+
+    private fun setButtonLogin() {
+        val email = binding.edEmail.text.toString()
+        val password = binding.edPass.text.toString()
+
+        if(email.isEmpty() and password.isEmpty()){
+            buttonEnabled(false)
+            showLongToast(getString(R.string.fill_data))
+        }else{
+            buttonEnabled(true)
+
+            val intentToHome = Intent(this, HomeActivity::class.java)
+            startActivity(intentToHome)
+            finish()
+        }
     }
 
     private fun setButtonToRegister(){
@@ -32,5 +90,13 @@ class LoginActivity : AppCompatActivity() {
             startActivity(intentToRegister)
             finish()
         }
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
+    }
+
+    private fun buttonEnabled(isEnabled: Boolean) {
+        binding.btnLogin.isEnabled = isEnabled
     }
 }
--- app/src/main/java/com/project/demiwatch/features/register/RegisterActivity.kt
+++ app/src/main/java/com/project/demiwatch/features/register/RegisterActivity.kt
@@ -3,7 +3,13 @@ package com.project.demiwatch.features.register
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.util.Patterns
+import android.view.View
 import com.project.demiwatch.R
+import com.project.demiwatch.core.utils.showLongToast
 import com.project.demiwatch.databinding.ActivityRegisterBinding
 import com.project.demiwatch.features.login.LoginActivity
 
@@ -15,20 +21,102 @@ class RegisterActivity : AppCompatActivity() {
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setButtonToLogin()
+        setupEditTextFormat()
 
         setButtonRegister()
+
+        setButtonToLogin()
+    }
+
+    private fun setupEditTextFormat() {
+        binding.edEmail.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                if (!Patterns.EMAIL_ADDRESS.matcher(p0!!).matches()) {
+                    binding.edEmail.error = getString(R.string.invalid_email)
+                    binding.btnRegister.isEnabled = false
+                } else binding.btnRegister.isEnabled =
+                    !(binding.edPass.text.isNullOrEmpty() || binding.edPassConfirm.text.isNullOrEmpty())
+            }
+        })
+
+        binding.edPass.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                if (p0.toString().length < 8) {
+                    binding.edPass.error = getString(R.string.invalid_password)
+                    binding.btnRegister.isEnabled = false
+                } else binding.btnRegister.isEnabled =
+                    !(binding.edEmail.text.isNullOrEmpty() || binding.edPassConfirm.text.isNullOrEmpty())
+            }
+        })
+
+        binding.edPassConfirm.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                if (p0.toString().length < 8) {
+                    binding.edPassConfirm.error = getString(R.string.invalid_password)
+                    binding.btnRegister.isEnabled = false
+                } else binding.btnRegister.isEnabled =
+                    !(binding.edPass.text.isNullOrEmpty() || binding.edEmail.text.isNullOrEmpty())
+            }
+        })
     }
 
     private fun setButtonRegister() {
+        val email = binding.edEmail.text.toString()
+        val password = binding.edPass.text.toString()
+        val passwordConfirm = binding.edPassConfirm.text.toString()
 
+        if (email.isEmpty() and password.isEmpty() and passwordConfirm.isEmpty()){
+            buttonEnabled(false)
+            showLongToast(getString(R.string.fill_data))
+        }else if(password != passwordConfirm){
+            buttonEnabled(false)
+            showLongToast(getString(R.string.pass_not_match))
+        }else{
+            buttonEnabled(true)
+            showLongToast(getString(R.string.complete_register_acc))
+
+            val intentToLogin = Intent(this, LoginActivity::class.java)
+            startActivity(intentToLogin)
+            finish()
+        }
     }
 
     private fun setButtonToLogin() {
         binding.tvToLogin.setOnClickListener {
             val intentToLogin = Intent(this, LoginActivity::class.java)
             startActivity(intentToLogin)
-            finish()
         }
+    }
+
+    private fun showLoading(isLoading: Boolean){
+        binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
+    }
+
+    private fun buttonEnabled(isEnabled: Boolean){
+        binding.btnRegister.isEnabled = isEnabled
     }
 }
