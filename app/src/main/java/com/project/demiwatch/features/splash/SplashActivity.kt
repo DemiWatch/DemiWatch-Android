a876b872f8375c169847c39e08775fda69ded63c
Krisna <krisnaerlangga08@gmail.com>
2023-10-15 15:56:39 +0700

    fix: Timber logging library

--- app/src/main/java/com/project/demiwatch/features/splash/SplashActivity.kt
+++ app/src/main/java/com/project/demiwatch/features/splash/SplashActivity.kt
@@ -1,14 +1,18 @@
 package com.project.demiwatch.features.splash
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.viewbinding.BuildConfig
 import com.project.demiwatch.R
 import com.project.demiwatch.databinding.ActivitySplashBinding
 import com.project.demiwatch.features.login.LoginActivity
+import timber.log.Timber
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySplashBinding
 
@@ -23,6 +27,10 @@ class SplashActivity : AppCompatActivity() {
             intentFromSplash.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
             finish()
         }, DELAY.toLong())
+
+        if(BuildConfig.DEBUG){
+            Timber.plant(Timber.DebugTree())
+        }
     }
 
     companion object{
