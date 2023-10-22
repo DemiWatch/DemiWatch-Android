bd44e8367bddcbfb0d6be0baf818436b1da54554
Krisna <krisnaerlangga08@gmail.com>
2023-10-22 15:23:20 +0700

    add: connect fill profile patient and user

--- app/src/main/java/com/project/demiwatch/features/fill_profile/patient/FillProfilePatientActivity.kt
+++ app/src/main/java/com/project/demiwatch/features/fill_profile/patient/FillProfilePatientActivity.kt
@@ -1,9 +1,12 @@
 package com.project.demiwatch.features.fill_profile.patient
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.project.demiwatch.R
+import com.project.demiwatch.core.utils.showLongToast
 import com.project.demiwatch.databinding.ActivityFillProfilePatientBinding
+import com.project.demiwatch.features.dashboard.MainActivity
 
 class FillProfilePatientActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFillProfilePatientBinding
@@ -12,5 +15,32 @@ class FillProfilePatientActivity : AppCompatActivity() {
 
         binding = ActivityFillProfilePatientBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setupButtonBack()
+
+        setupSaveButton()
+    }
+
+    private fun setupSaveButton() {
+        val name = binding.edPatientFullName.text.toString()
+        val age = binding.edPatientAge.text.toString()
+        val status = binding.dropdownMenu.text.toString()
+        val patientNotes = binding.edPatientNotes.text.toString()
+        val watchId = binding.edPatientWatchId.text.toString()
+        val homeAddress = binding.edPatientAddressHome.text.toString()
+        val destinationAddress = binding.edPatientAddressDestination.text.toString()
+
+        if(name.isNotEmpty() and age.isNotEmpty() and status.isNotEmpty() and patientNotes.isNotEmpty() and watchId.isNotEmpty() and homeAddress.isNotEmpty() and destinationAddress.isNotEmpty()){
+            val intentToMain = Intent(this, MainActivity::class.java)
+            startActivity(intentToMain)
+        }else{
+            showLongToast(getString(R.string.fill_data))
+        }
+    }
+
+    private fun setupButtonBack() {
+        binding.btnBack.setOnClickListener {
+            finish()
+        }
     }
 }
--- app/src/main/java/com/project/demiwatch/features/fill_profile/user/FillProfileUserActivity.kt
+++ app/src/main/java/com/project/demiwatch/features/fill_profile/user/FillProfileUserActivity.kt
@@ -1,9 +1,12 @@
 package com.project.demiwatch.features.fill_profile.user
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.project.demiwatch.R
+import com.project.demiwatch.core.utils.showLongToast
 import com.project.demiwatch.databinding.ActivityFillProfileUserBinding
+import com.project.demiwatch.features.fill_profile.patient.FillProfilePatientActivity
 
 class FillProfileUserActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFillProfileUserBinding
@@ -15,6 +18,25 @@ class FillProfileUserActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setupButtonBack()
+
+        setupSaveButton()
+    }
+
+    private fun setupSaveButton() {
+        binding.btnSave.setOnClickListener {
+            val name = binding.edProfileName.text.toString()
+            val phone = binding.edProfileTelp.text.toString()
+            val status = binding.dropdownMenu.text.toString()
+            val safeRadius = binding.edProfileSafeRadius.text.toString()
+
+
+            if(name.isNotEmpty() and phone.isNotEmpty() and status.isNotEmpty() and safeRadius.isNotEmpty()){
+                val intentToFillProfile = Intent(this, FillProfilePatientActivity::class.java)
+                startActivity(intentToFillProfile)
+            }else{
+                showLongToast(getString(R.string.fill_data))
+            }
+        }
     }
 
     private fun setupButtonBack() {
