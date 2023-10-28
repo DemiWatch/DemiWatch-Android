16d1fefb13e25fd2610dcca90cc83324b3707d05
Krisna <krisnaerlangga08@gmail.com>
2023-10-28 21:14:06 +0700

    add: profile layout

--- app/src/main/java/com/project/demiwatch/features/profile/ProfileFragment.kt
+++ app/src/main/java/com/project/demiwatch/features/profile/ProfileFragment.kt
@@ -5,17 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import com.project.demiwatch.R
+import com.project.demiwatch.core.utils.showToast
 import com.project.demiwatch.databinding.FragmentHomeBinding
 import com.project.demiwatch.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
     private var _binding: FragmentProfileBinding? = null
     private val binding get() =_binding!!
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,9 +24,40 @@ class ProfileFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupSettingsPopUp()
+    }
+
+    private fun setupSettingsPopUp() {
+        binding.btnMenu.setOnClickListener {
+            val popUpMenu = PopupMenu(requireContext(), binding.btnMenu)
+            popUpMenu.menuInflater.inflate(R.menu.profile_settings, popUpMenu.menu)
+
+            popUpMenu.setOnMenuItemClickListener {
+                when(it.itemId){
+                    R.id.settingsEdit ->{
+                        requireContext().showToast("Edit")
+                        true
+                    }
+                    R.id.settingsLogOut ->{
+                        requireContext().showToast("Logout")
+                        true
+                    }
+                    else -> false
+                }
+            }
+
+            popUpMenu.show()
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
 
         _binding = null
     }
+
+
 }
