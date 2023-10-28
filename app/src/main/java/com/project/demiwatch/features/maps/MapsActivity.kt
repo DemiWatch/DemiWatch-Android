00822866ae05196232bc4e56756eb2a4cbe3218c
Krisna <krisnaerlangga08@gmail.com>
2023-10-28 14:13:13 +0700

    add: mapbox map view

--- app/src/main/java/com/project/demiwatch/features/maps/MapsActivity.kt
+++ app/src/main/java/com/project/demiwatch/features/maps/MapsActivity.kt
@@ -3,12 +3,28 @@ package com.project.demiwatch.features.maps
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.project.demiwatch.R
+import com.mapbox.maps.MapView
+import com.mapbox.maps.Style
+import com.project.demiwatch.databinding.ActivityMapsBinding
+
+var mapView: MapView? = null
 
 class MapsActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityMapsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_maps)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupActionBar()
+
+        mapView = binding.mapView
+        mapView?.getMapboxMap()?.loadStyleUri(Style.MAPBOX_STREETS)
+    }
+
+    private fun setupActionBar(){
+        supportActionBar?.hide()
     }
 }
